18b1034abc0271268393319a6b098c89a427e5b5
AlainB888 <125298634+AlainB888@users.noreply.github.com>
2024-01-03 22:54:32 +0200

    Update itemPage.js

--- Mag React Project/client/src/pages/itemPage.js
+++ Mag React Project/client/src/pages/itemPage.js
@@ -1,15 +1,48 @@
-import Navbar from "../components/navbar";
-import ItemInfoCard from "../components/itemInfoCard";
-import Footer from "../components/footer";
+import { useParams } from 'react-router-dom';
+import Navbar from '../components/navbar';
+import ItemInfoCard from '../components/itemInfoCard';
+import Footer from '../components/footer';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
+function ItemPage() {
+  const { itemId } = useParams();
+  const [item, setItem] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-function ItemPage(){
-    return( 
-        <div>
-            <Navbar/>
-            <ItemInfoCard/>
-            <Footer/>
-            </div>
-    );
+  useEffect(() => {
+    const fetchItemDetails = async () => {
+      try {
+        const res = await axios.get(`http://localhost:8800/item/${itemId}`);
+        setItem(res.data);
+        setLoading(false);
+      } catch (err) {
+        console.error('Error fetching item details:', err); // Log the specific error
+        setError('Error fetching item details. Please check the console for more information.');
+        setLoading(false);
+      }
+      
+    };
+
+    fetchItemDetails();
+  }, [itemId]);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  return (
+    <div>
+      <Navbar />
+      <ItemInfoCard item={item} />
+      <Footer/>
+    </div>
+  );
 }
+
 export default ItemPage;
