9f87f990e34339e6f14d5389c9f14527f9427213
Michel khairallah <michelkhairallah67@outlook.com>
2024-01-03 14:52:17 +0200

    v 1.0

---
+++ Mag React Project/client/src/pages/pages/itemPage.js
@@ -0,0 +1,16 @@
+import Navbar from "../components/navbar";
+import ItemCard from "../components/itemCard";
+
+
+function ItemPage(){
+    return( 
+        <div>
+            <Navbar/>
+            <ItemCard/>
+        
+
+
+        </div>
+    );
+}
+export default ItemPage;
---
+++ Mag React Project/client/src/pages/pages/mainPage.js
@@ -0,0 +1,11 @@
+import Navbar from "../components/navbar";
+
+function MainPage(){
+    return(
+        <div>
+            <Navbar/>
+            <h1>Main Page</h1>
+        </div>
+    );
+}
+export default MainPage;
